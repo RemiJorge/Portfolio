d084a768da682d3655da71dc3c28011d1a9d2397
remi.jorge <remi.jorge@etu.umontpellier.fr>
2025-05-20 21:11:44 +0200

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,35 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import Navbar from '@/components/NavBar';
+import "./globals.css";
+import type { Metadata } from "next";
+import Script from "next/script";
+import Navbar from "@/components/NavBar";
 
 export const metadata: Metadata = {
   title: "Remi's Portfolio",
   description: "Software & AI Engineer Portfolio",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-87N7X2YXWX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-87N7X2YXWX');
+          `}
+        </Script>
+      </head>
       <body className="dark-theme">
         <Navbar />
         <main>{children}</main>
